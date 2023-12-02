3f8cc9eaf88c716ef2ed7bbbc73ef2605a1235a0
FlavinouTheOne <flavien.dumas63@gmail.com>
2023-12-02 13:56:40 +0100

    done 90% of sideBar

--- src/components/CardSelf.jsx
+++ src/components/CardSelf.jsx
@@ -6,7 +6,7 @@ function CardSelf() {
         <div className="divHead center">
             <img src="../src/assets/pdpJapon.jpeg" alt="PDP" />
             <h2>Flavien Dumas</h2>
-            <div className="center">Développeur Fullstack <br/> Junior</div>
+            <div className="center">Concepteur/Développeur Fullstack <br/> Junior</div>
         </div>
     )
   }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,18 @@
 import CardSelf from "./CardSelf";
+import { DiHtml5 } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { BiLogoTypescript } from "react-icons/bi";
+import { DiReact } from "react-icons/di";
+import { DiGit } from "react-icons/di";
+import { FaDocker } from "react-icons/fa";
+import { TbBrandNodejs } from "react-icons/tb";
+import { SiPhp } from "react-icons/si";
+import { GrGraphQl } from "react-icons/gr";
+import { TbApi } from "react-icons/tb";
+import { DiGithubBadge } from "react-icons/di";
+import { IoLogoLinkedin } from "react-icons/io";
+import { MdPhoneEnabled } from "react-icons/md";
+import { IoMdMail } from "react-icons/io";
 
 function SideBar() {
 
@@ -15,14 +29,55 @@ function SideBar() {
                     <div>23</div>
                 </div>
             </div>
-            <div className="divLangages">
-
+            <div className="divInfo">
+                <h2>Frontend</h2>
+                <div className="center">
+                    <div>React</div>
+                    <div className="center">
+                        <DiReact className="icon"/>
+                    </div>
+                </div>
+                <div className="center">
+                    <div>Html/Css/TS</div>
+                    <div className="center">
+                        <DiHtml5 className="icon"/>
+                        <DiCss3 className="icon"/>
+                        <BiLogoTypescript className="icon"/>
+                    </div>
+                </div>
             </div>
-            <div className="divSkills">
-
+            <div className="divInfo">
+                <h2>Backend</h2>
+                <div className="center">
+                    <div>NodeJs/Php</div>
+                    <div className="center">
+                        <TbBrandNodejs className="icon"/>
+                        <SiPhp className="icon"/>
+                    </div>
+                </div>
+                <div className="center">
+                    <div>GraphQL/REST</div>
+                    <div className="center">
+                        <GrGraphQl className="icon"/>
+                        <TbApi className="icon"/>
+                    </div>
+                </div>
             </div>
-            <div className="divTechno">
-
+            <div className="divInfo">
+                <h2>DevOps</h2>
+                <div className="center">
+                    <div>Git/Docker</div>
+                    <div className="center">
+                        <DiGit className="icon"/>
+                        <FaDocker className="icon"/>
+                    </div>
+                </div>
+            </div>
+            <div className="center divContact">
+                <a href=""><DiGithubBadge className="icon"/></a>
+                <a href=""><IoLogoLinkedin className="icon"/></a>
+                <MdPhoneEnabled className="icon"/>
+                <button><IoMdMail className="icon"/></button>
             </div>
         </div>
     )
